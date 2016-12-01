01d0ea935c97be89551062c38df904d9d4d09138
Mark Stumbris <Mark@birdash.com>
2016-12-01 14:46:22 -0800

    First Commit

---
+++ Voyager/Voyager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voyager
+//
+//  Created by Stum on 12/1/16.
+//  Copyright © 2016 BirdAsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
